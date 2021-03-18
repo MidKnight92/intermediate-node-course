509234e2e6d2d5b461ad838cfac0418478166973
Stephanie Viveros <sviveros05@gmail.com>
2021-03-17 20:23:15 -0500

    add user model and connect to db

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
 
-app.use(bodyParser.json());
-
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
+
+const User = require('./models/User');
+mongoose.connect('mongodb://localhost/userData', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 // CREATE
 app.post('/users',(req,res)=>{
